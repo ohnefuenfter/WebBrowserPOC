a40b2bfc9caef5572907f9c9e34f6dc128eaf05b
Roman Ananyev <roman_ananyev@hotmail.com>
2017-11-27 10:07:54 +0200

    web browser POC final

--- WebBrowserPOC/MainWindow.xaml.cs
+++ WebBrowserPOC/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Navigation;
 using mshtml;
 
@@ -9,7 +10,7 @@ namespace WebBrowserPOC
     /// </summary>
     public partial class MainWindow : Window
     {
-        private const string style = ".btn-group .button {" +
+        private const string style = ".frozenButtons .button {" +
                                      "background-color: white; " +
                                      "border: 1px solid gray; " +
                                      "color: black; " +
@@ -21,10 +22,10 @@ namespace WebBrowserPOC
                                      "cursor: pointer; " +
                                      "     width: 7%" +
                                      "}" +
-                                     ".btn-group .button:not(:last-child) { " +
+                                     ".frozenButtons .button:not(:last-child) { " +
                                      "border-right: none; /* Prevent double borders */ " +
                                      "}" +
-                                     ".btn-group .button:hover { " +
+                                     ".frozenButtons .button:hover { " +
                                      "background-color: #e7e7e7; " +
                                      "}";
         public MainWindow()
@@ -33,12 +34,12 @@ namespace WebBrowserPOC
 
             Browser.NavigateToString("<html><head></head><body> " +
                                      "<p>Click the 'Try it' button to toggle between hiding and showing the DIV element:</p> " +
-                                     "<div class='btn-group'><hr style='display:inline-block; width:85%;' /><button class='button' id='toggleVis1' onclick = \"toggleVisibility('myDIV1')\">&#x2796;</button><button class='button' id='toggleWeb1' onclick = \"toggleWebVisibility('myDIV1')\">&#x2713;</button></div> " +
+                                     "<div class='frozenButtons'><hr style='display:inline-block; width:85%;' /><button class='button' id='toggleVis1' onclick = \"toggleVisibility('myDIV1')\">&#x2796;</button><button class='button' id='toggleWeb1' onclick = \"toggleWebVisibility('myDIV1')\">&#x2713;</button></div> " +
                                      "<p style='clear:both'><div style='background-color:white' id = 'myDIV1'>This is my DIV 1 element.</div></p>" +
-                                     "<div class='btn-group'><hr style='display:inline-block; width:85%;' /><button class='button' id='toggleVis2' onclick = \"toggleVisibility('myDIV2')\">&#x2795;</button><button class='button' id='toggleWeb2' onclick = \"toggleWebVisibility('myDIV2')\">&#x2717;</button></div>" +
+                                     "<div class='frozenButtons'><hr style='display:inline-block; width:85%;' /><button class='button' id='toggleVis2' onclick = \"toggleVisibility('myDIV2')\">&#x2795;</button><button class='button' id='toggleWeb2' onclick = \"toggleWebVisibility('myDIV2')\">&#x2717;</button></div>" +
                                      "<p style='clear:both'><div style='display:none;background-color:#e7e7e7' id = 'myDIV2'>This is my DIV 2 element.</div></p>" +
                                      "<p><b>Note:</b> The element will not take up any space when the display property set to 'none'.</p> " +
-                                     "<script> " +
+                                     "<script id=\"frozenButtonScripts\"> " +
                                      "function toggleVisibility(elementId) " +
                                      "{" +
                                      "var x = document.getElementById(elementId); " +
@@ -81,6 +82,7 @@ namespace WebBrowserPOC
         {
             var mshtmlDoc = Browser.Document as HTMLDocument;
             if (mshtmlDoc == null) return;
+            
             foreach (var lnk in mshtmlDoc.getElementsByTagName("BUTTON"))
             {
                 var clickable = lnk as HTMLButtonElement;
@@ -88,10 +90,28 @@ namespace WebBrowserPOC
                 if (events == null) continue;                
                 events.onclick += Link2Clicked;
             }
+            var doc2event = mshtmlDoc as HTMLDocumentEvents2_Event;
+            if (doc2event != null)
+            {               
+                doc2event.onfocusin += FocusInContextMenu;
+            }
             var doc2 = mshtmlDoc as IHTMLDocument2;
             if (doc2 == null) return;
             var ss = doc2.createStyleSheet("", 0);
             ss.cssText = style;
+        }
+
+        private void FocusInContextMenu(IHTMLEventObj pevtobj)
+        {
+            var mshtmlDoc = Browser.Document as HTMLDocument;
+            var doc2event = mshtmlDoc as HTMLDocumentEvents2_Event;
+            if (doc2event != null)
+            {
+                doc2event.oncontextmenu -= OpenContextMenu;
+                doc2event.onfocusin -= FocusInContextMenu;
+                doc2event.oncontextmenu += OpenContextMenu;
+                doc2event.onfocusin += FocusInContextMenu;
+            }
         }
 
         private bool Link2Clicked(IHTMLEventObj pevtobj)
@@ -114,6 +134,20 @@ namespace WebBrowserPOC
                 webstate[index] = !webstate[index];
             }
             return true;
+        }
+
+        bool OpenContextMenu(IHTMLEventObj pEvtObj)
+        {
+            WbShowContextMenu(pEvtObj as ContextMenu);
+            return false;
+        }
+
+        public void WbShowContextMenu(ContextMenu placementTarget)
+        {
+            ContextMenu cm = FindResource("MnuCustom") as ContextMenu;
+            if (cm == null) return;
+            cm.PlacementTarget = placementTarget;
+            cm.IsOpen = true;
         }
 
     }
